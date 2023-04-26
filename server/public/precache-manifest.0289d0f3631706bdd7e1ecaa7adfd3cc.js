9f8b25dffa220ad156ea2ade78dde700f8ee99da
VerySeriousHireableAccount <phantranh2206@gmail.com>
2023-04-26 03:59:52 -0400

    create public folder in server folder

---
+++ server/public/precache-manifest.0289d0f3631706bdd7e1ecaa7adfd3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee0bf464ea117c6b182ec279368e484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5caf2f0304c98c1d7f64",
+    "url": "/static/css/2.5a223145.chunk.css"
+  },
+  {
+    "revision": "5caf2f0304c98c1d7f64",
+    "url": "/static/js/2.2a17a81f.chunk.js"
+  },
+  {
+    "revision": "4c88f1e2cb18396c0169",
+    "url": "/static/js/main.00b6cdcb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
